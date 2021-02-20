8c7c947fa3a86a10910b7c47e8ad39f5bc8f97dc
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-02-19 21:47:04 -0500

    Updated css, html, and js.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//variables for existing and created elements 
+//variables for newly created elements 
 let listEl = document.createElement("ol");
 let li1 = document.createElement("li");
 let li2 = document.createElement("li");
@@ -6,18 +6,22 @@ let li3 = document.createElement("li");
 let btN1 = document.createElement("button");
 let btN2 = document.createElement("button");
 let btN3 = document.createElement("button");
+
+//variable for the score and time elements
+let scoreTag = document.createElement("h2");
+let timeTag = document.createElement("h2");
+
 let pTags = document.querySelector("p")
 let h1Tag = document.querySelector("h1")
-let h2Tags = document.createElement("h2");
+let feedBack = document.getElementById("feedback")
+
 let btN = document.querySelector("button")
 let sectionTag = document.querySelector(".game-card")
 
 //variables for finalCountDown
 let secondsLeft = 60;
-
-let totalScore; //for end of quiz score
-let points = 10; //points for each correctly answered question
-let correctAnswer = true; //for question answers
+//variable for starting points
+let points = 0;
 
 //begin game button
 let beginBtn = document.querySelector("#beginQuiz");
@@ -31,9 +35,13 @@ function startQuiz() {
     h1Tag.textContent = " ";
     btN.textContent = " ";
     btN.hidden = " ";
-    sectionTag.prepend(h2Tags);
-    h2Tags.textContent = "Time: ";
-    h2Tags.setAttribute("style", "font-family:monospace; color:rgb(214, 176, 119); margin-left:-1%; margin-top: 3%; margin-bottom: -4%; text-align: center; font-size:22px;");
+    
+    sectionTag.prepend(scoreTag);
+    sectionTag.prepend(timeTag);
+    scoreTag.setAttribute("style", "font-family:monospace; color:rgb(214, 176, 119);");
+    timeTag.setAttribute("style", "font-family:monospace; color:rgb(214, 176, 119);");
+    feedBack.setAttribute("style", "color:rgb(214, 176, 119);")
+
     pTags.appendChild(listEl)
     listEl.appendChild(li1);
     listEl.appendChild(li2);
@@ -45,101 +53,175 @@ function startQuiz() {
     for (let i = 0; i < buttonZ.length; i++)
         buttonZ[i].setAttribute("class", "shinyBtn")
     
-    h1Tag.setAttribute("style", "font-size: 25px; margin-top:13%;");
-    listEl.setAttribute("style", "text-align:center; margin:inherit; margin-top:-4%;");
+    h1Tag.setAttribute("style", "font-size: 25px;");
+    listEl.setAttribute("style", "text-align:center;");
     
     let quizItems = [li1, li2, li3];
     for (let i = 0; i < quizItems.length; i++)
         quizItems[i].setAttribute("style", "line-height:2.5; text-align:left;");
-    firstQuestion();
     finalCountDown();
+    firstQuestion();
 }
 
 function finalCountDown() {
-    let stopWatch = document.querySelector("h2");
     let timerInterval = setInterval(function () {
         secondsLeft--;
-        stopWatch.textContent = "Time: " + secondsLeft;
-
-        if (secondsLeft === 0) {
+        timeTag.textContent = "Time: " + secondsLeft;
+        
+        if (secondsLeft <=0) {
+            secondsLeft = 0;
             clearInterval(timerInterval);
-            //if else for time losses for wrong answers and end quiz?
+            endQuiz();
         }
 
     }, 1000);
 }
 
-function scoreBoard() {
-    if (correctAnswer === true) {
-        points++;
-    }else {(correctAnswer !== true)
-        points--;
+function correctAnswer() {
+    scoreTag.textContent = "Score: " + points;
+    points+=10;
 }
+
+function wrongAnswer (){
+    secondsLeft = secondsLeft - 10;
+    timeTag.textContent = "Time: " + secondsLeft;
+    if (secondsLeft <= 0) {
+        secondsLeft = 0;
+    }
 }
 
 function firstQuestion() {
-    h1Tag.textContent = "What is the correct syntax for 'calling to' a function?";
+    h1Tag.textContent = "1. What is the correct syntax for 'calling to' a function?";
     btN1.textContent = "callingThisFunctionOut();";
     btN2.textContent = "callingThisFunctionOut[];";
     btN3.textContent = "callingThisFunctionOut = 'Come out and play!';";
-    
-    btN1.addEventListener("click", secondQuestion);
-    btN2.addEventListener("click", secondQuestion);
-    btN3.addEventListener("click", secondQuestion);
+
+    btN1.addEventListener("click", function() {
+        feedBack.textContent = "Correct!";
+        correctAnswer();
+        console.log("correct");
+        secondQuestion();
+    })
+    btN2.addEventListener("click", function() {
+        feedBack.textContent = "Wrong Answer!";
+        wrongAnswer();
+        secondQuestion();
+    })
+
+    btN3.addEventListener("click", function() {
+        feedBack.textContent = "Wrong Answer!";
+        wrongAnswer();
+        secondQuestion();
+    })
 }
 
-
 function secondQuestion() {
-    h1Tag.textContent = "What sets or returns the text content of the specified node?";
+    h1Tag.textContent = "2. What sets or returns the text content of the specified node?";
     btN1.textContent = "textDisplay";
     btN2.textContent = "textContent";
     btN3.textContent = "textShow";
     
-    btN1.addEventListener("click", thirdQuestion);
-    btN2.addEventListener("click", thirdQuestion);
-    btN3.addEventListener("click", thirdQuestion);
+    btN1.addEventListener("click", function() {
+    feedBack.textContent = "Wrong Answer!";
+        wrongAnswer();
+        thirdQuestion();
+    })
+    btN2.addEventListener("click", function() {
+        feedBack.textContent = "Correct!";
+        correctAnswer();
+        console.log("correct");
+        thirdQuestion();
+    })
+
+    btN3.addEventListener("click", function() {
+        feedBack.textContent = "Wrong Answer!";
+        wrongAnswer();
+        thirdQuestion();
+    })
 }
 
-
 function thirdQuestion() {
-    h1Tag.textContent = "A boolean represents one of two values:";
+    h1Tag.textContent = "3. A boolean represents one of two values:";
     btN1.textContent = "Left or right.";
     btN2.textContent = "Equal to or not equal to.";
     btN3.textContent = "True or false.";
     
-    btN1.addEventListener("click", fourthQuestion);
-    btN2.addEventListener("click", fourthQuestion);
-    btN3.addEventListener("click", fourthQuestion);
+    btN1.addEventListener("click",  function() {
+        feedBack.textContent = "Wrong Answer!";
+        wrongAnswer();
+        fourthQuestion();
+    })
+    btN2.addEventListener("click",  function() {
+        feedBack.textContent = "Wrong Answer!";
+        wrongAnswer();
+        fourthQuestion();
+    })
+    btN3.addEventListener("click", function() {
+        feedBack.textContent = "Correct!";
+        correctAnswer();
+        console.log("correct");
+        fourthQuestion();
+    })
 }
 
 function fourthQuestion() {
-    h1Tag.textContent = "What is the DOM?";
+    h1Tag.textContent = "4. What is the DOM?";
     btN1.textContent = "Differential Object Method.";
     btN2.textContent = "Document Object Model.";
     btN3.textContent = "Dependent Object Material.";
     
-    btN1.addEventListener("click", fifthQuestion);
-    btN2.addEventListener("click", fifthQuestion);
-    btN3.addEventListener("click", fifthQuestion);
+    btN1.addEventListener("click", function() {
+        feedBack.textContent = "Wrong Answer!";
+        wrongAnswer();
+        fifthQuestion();
+    })
+
+    btN2.addEventListener("click", function() {
+        feedBack.textContent = "Correct!";
+        correctAnswer();
+        console.log("correct");
+        fifthQuestion();
+    })
+    
+    btN3.addEventListener("click", function() {
+        feedBack.textContent = "Wrong Answer!";
+        wrongAnswer();
+        fifthQuestion();
+    })
 }
 
 function fifthQuestion() {
-    h1Tag.textContent = "What creates an instance of the element of a specified tag?";
+    h1Tag.textContent = "5. What creates an instance of the element of a specified tag?";
     btN1.textContent = "document.createElement";
     btN2.textContent = "document.manifestElement";
     btN3.textContent = "document.addElement";
     
-    btN1.addEventListener("click", sixthQuestion);
-    btN2.addEventListener("click", sixthQuestion);
-    btN3.addEventListener("click", sixthQuestion);
+    btN1.addEventListener("click", function() {
+        feedBack.textContent = "Correct!";
+        correctAnswer();
+        console.log("correct");
+        endQuiz();
+    })
+
+    btN2.addEventListener("click", function() {
+        feedBack.textContent = "Wrong Answer!";
+        wrongAnswer();
+        endQuiz();
+    })
+    
+    btN3.addEventListener("click", function() {
+        feedBack.textContent = "Wrong Answer!";
+        wrongAnswer();
+        endQuiz();
+    })
 }
 
 function sixthQuestion() {
-    h1Tag.textContent = "Which of these is a example of an string array?";
+    h1Tag.textContent = "6. Which of these is a example of an string array?";
     btN1.textContent = "[12345];";
     btN2.textContent = "'raspbery cake';";
     btN3.textContent = "['platypus', 'sugarglider', 'mongoose'];";
-    1
+    
     btN1.addEventListener("click", endQuiz);
     btN2.addEventListener("click", endQuiz);
     btN3.addEventListener("click", endQuiz);
@@ -151,5 +233,8 @@ function endQuiz() {
     btN2.hidden = " ";
     btN3.hidden = " ";
     listEl.textContent = " ";
+    feedBack.textContent = " ";
 // high scores and form box for initials
+//imput element, submit btn, and access to local storage
+
 }
